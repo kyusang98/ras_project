fdd48eb686d709dd757646b0e283ada3b6fcdb96
Code Beginner <83444385+kyusang98@users.noreply.github.com>
2023-07-07 15:06:05 +0900

    Update dictdb1.py for security

--- dictdb1.py
+++ dictdb1.py
@@ -16,8 +16,8 @@ def write_data(stockname, date, value, dynamodb):
     print('--------')
 
 dynamodb = boto3.resource('dynamodb',
-                          aws_access_key_id = "AKIAYBRKZA2B45R472XE",
-                          aws_secret_access_key="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",
+                          aws_access_key_id = "엑세스키",
+                          aws_secret_access_key="비밀 엑세스키",
                           region_name = "us-east-2", 
                           #DynamoDB의 리전(region)을 "us-east-2" 미국 동부 (오하이오)로 설정하고 거기에 table을 생성하였으므로
                           #"ap-northeast-2" 아시아 태평양(서울)로 설정하고 코드 돌리면 당연히 table이 없다는 오류가 발생한다.
